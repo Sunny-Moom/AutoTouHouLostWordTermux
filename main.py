7d224e4edc6c2c9e3644451c1c84a5d68295c60c
Sunny-Moom <khgkhh@foxmail.com>
2023-05-06 13:09:01 +0800

    小更新

--- main.py
+++ main.py
@@ -2,62 +2,15 @@ import os
 import subprocess
 import sys
 import time
-from PIL import Image
 import cv2
 import numpy as np
-## import pytesseract
- 
-# 关卡名
-## play_text = '血'
 
-"""
-# 定义一个函数，接受一个图片路径和一个词作为参数
-def get_word_coordinates(image_path, word):
-    # 读取图片文件
-    img = Image.open(image_path)
-
-    # 设置tesseract命令行参数
-    config = "-l chi_sim --oem 1 --psm 3"
-
-    # 调用image_to_data方法，返回一个字典列表
-    data = pytesseract.image_to_data(img, config=config, output_type=pytesseract.Output.DICT)
-
-    # 遍历字典列表，检查是否有匹配的词
-    for i in range(len(data["text"])):
-        if data["text"][i] == word:
-            # 返回词的坐标，格式为(x,y,w,h)
-            return data["left"][i], data["top"][i]
-
-    # 如果没有匹配，返回None
-    return None
-"""
 
 # 使用adb截图
 def adb_image():
     subprocess.call("adb shell screencap -p /sdcard/screen.png", shell=True, stdout=subprocess.DEVNULL)
     subprocess.call("adb pull /sdcard/screen.png ./img/screen.png", shell=True, stdout=subprocess.DEVNULL)
     
-"""
-# OCR识别点击
-def find_and_click(text):
-    while True:
-        # 加载图像
-
-        adb_image()
-        center = get_word_coordinates('./img/screen.png', text)
-        if center is not None:
-            print("\033[32m" + "找到匹配图像，中心点坐标为：" + "\033[0m", center)
-            sys.stdout.write("\033[F")  # 光标上移一行
-            sys.stdout.write("\033[K")  # 清除当前行
-            # 模拟点击
-            subprocess.call("adb shell input tap {} {}".format(center[0], center[1]), shell=True, stdout=subprocess.DEVNULL)
-            break
-        else:
-            print("\033[31m" + f"未找到匹配图像，3秒后重新查找" + "\033[0m")
-            time.sleep(3)
-            sys.stdout.write("\033[F")  # 光标上移一行
-            sys.stdout.write("\033[K")  # 清除当前行
-"""
 
 # 图像识别并点击
 def match_image(template_file, num, cold):
